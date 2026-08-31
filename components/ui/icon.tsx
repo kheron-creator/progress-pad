b058ffd5a24f0a5a530e70b5ba69c016c5d52023
Fatima Waheed <fatimawhd.dev@gmail.com>
2026-08-31 21:01:08 +0500

    Updated icons and icon mark

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import type { ComponentType } from "react";
 import {
   BarbellIcon as PhosphorBarbellIcon,
   BellIcon as PhosphorBellIcon,
+  BookOpenIcon as PhosphorBookOpenIcon,
+  BrainIcon as PhosphorBrainIcon,
+  BriefcaseIcon as PhosphorBriefcaseIcon,
+  CalendarBlankIcon as PhosphorCalendarBlankIcon,
   CameraIcon as PhosphorCameraIcon,
   CaretLeftIcon,
   CaretRightIcon,
@@ -11,15 +16,22 @@ import {
   ChartLineUpIcon as PhosphorChartLineUpIcon,
   CheckCircleIcon as PhosphorCheckCircleIcon,
   CheckIcon as PhosphorCheckIcon,
+  ChecksIcon as PhosphorChecksIcon,
   DotsSixVerticalIcon,
+  HeadCircuitIcon as PhosphorHeadCircuitIcon,
+  HeartIcon as PhosphorHeartIcon,
+  LightbulbIcon as PhosphorLightbulbIcon,
   LightningIcon as PhosphorLightningIcon,
   ListBulletsIcon as PhosphorListBulletsIcon,
   ListIcon as PhosphorListIcon,
   MagnifyingGlassIcon as PhosphorMagnifyingGlassIcon,
+  MicrophoneIcon as PhosphorMicrophoneIcon,
   MoonIcon as PhosphorMoonIcon,
   NoteIcon as PhosphorNoteIcon,
   PencilSimpleIcon,
   PlusIcon as PhosphorPlusIcon,
+  QuotesIcon as PhosphorQuotesIcon,
+  SmileyIcon as PhosphorSmileyIcon,
   SparkleIcon as PhosphorSparkleIcon,
   SquaresFourIcon,
   StarIcon as PhosphorStarIcon,
@@ -30,220 +42,105 @@ import {
   EnvelopeSimpleIcon,
   EyeIcon as PhosphorEyeIcon,
   EyeSlashIcon as PhosphorEyeSlashIcon,
+  Info as PhosphorInfoIcon,
   LockSimpleIcon,
   UploadSimpleIcon,
   PaperPlaneTiltIcon,
   UserIcon as PhosphorUserIcon,
+  UsersThreeIcon as PhosphorUsersThreeIcon,
   XIcon,
   type IconProps as PhosphorIconProps,
 } from "@phosphor-icons/react";
 
 import { cn } from "@/lib/utils/cn";
 
-export type IconProps = PhosphorIconProps;
+export const iconSize = {
+  xs: 20,
+  sm: 26,
+  md: 36,
+  lg: 40,
+} as const;
+
+export type IconSizeName = keyof typeof iconSize;
+
+export type IconProps = Omit<PhosphorIconProps, "size"> & {
+  size?: PhosphorIconProps["size"] | IconSizeName;
+};
+
+function resolveIconSize(size: IconProps["size"]) {
+  if (size == null) {
+    return iconSize.xs;
+  }
+
+  if (typeof size === "string" && size in iconSize) {
+    return iconSize[size as IconSizeName];
+  }
+
+  return size;
+}
 
 function iconClass(className: string | undefined) {
   return cn("shrink-0", className);
 }
 
-export function CheckIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorCheckIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
+function withIcon(Icon: ComponentType<PhosphorIconProps>) {
+  return function BrandIcon({ className, size, weight = "regular", ...props }: IconProps) {
+    return (
+      <Icon
+        aria-hidden
+        size={resolveIconSize(size)}
+        weight={weight}
+        className={iconClass(className)}
+        {...props}
+      />
+    );
+  };
 }
 
-export function ChevronDownIcon({ className, ...props }: IconProps) {
-  return (
-    <CaretDownIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function StarIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorStarIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function CloseIcon({ className, ...props }: IconProps) {
-  return <XIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />;
-}
-
-export function UserIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorUserIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function SunIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorSunIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function MoonIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorMoonIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function SparkleIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorSparkleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function LightningIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorLightningIcon aria-hidden size={16} weight="fill" className={iconClass(className)} {...props} />
-  );
-}
-
-export function TrashIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorTrashIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function BellIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorBellIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function MenuIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorListIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function SearchIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorMagnifyingGlassIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function PlusIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorPlusIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function PencilIcon({ className, ...props }: IconProps) {
-  return (
-    <PencilSimpleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function DragHandleIcon({ className, ...props }: IconProps) {
-  return (
-    <DotsSixVerticalIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function ChevronLeftIcon({ className, ...props }: IconProps) {
-  return (
-    <CaretLeftIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function ChevronRightIcon({ className, ...props }: IconProps) {
-  return (
-    <CaretRightIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function GridIcon({ className, ...props }: IconProps) {
-  return (
-    <SquaresFourIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function ListBulletsIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorListBulletsIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function CheckCircleIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorCheckCircleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function ChatIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorChatIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function UploadIcon({ className, ...props }: IconProps) {
-  return (
-    <UploadSimpleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function CameraIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorCameraIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function SendIcon({ className, ...props }: IconProps) {
-  return (
-    <PaperPlaneTiltIcon aria-hidden size={16} weight="fill" className={iconClass(className)} {...props} />
-  );
-}
-
-export function BarbellIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorBarbellIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function NoteIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorNoteIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function TargetIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorTargetIcon aria-hidden size={16} weight="fill" className={iconClass(className)} {...props} />
-  );
-}
-
-export function ChartLineIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorChartLineUpIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function SunHorizonIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorSunHorizonIcon aria-hidden size={16} weight="fill" className={iconClass(className)} {...props} />
-  );
-}
-
-export function EnvelopeIcon({ className, ...props }: IconProps) {
-  return (
-    <EnvelopeSimpleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function LockIcon({ className, ...props }: IconProps) {
-  return (
-    <LockSimpleIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function EyeIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorEyeIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
-
-export function EyeSlashIcon({ className, ...props }: IconProps) {
-  return (
-    <PhosphorEyeSlashIcon aria-hidden size={16} weight="regular" className={iconClass(className)} {...props} />
-  );
-}
+export const CheckIcon = withIcon(PhosphorCheckIcon);
+export const ChecksIcon = withIcon(PhosphorChecksIcon);
+export const ChevronDownIcon = withIcon(CaretDownIcon);
+export const StarIcon = withIcon(PhosphorStarIcon);
+export const CloseIcon = withIcon(XIcon);
+export const UserIcon = withIcon(PhosphorUserIcon);
+export const UsersThreeIcon = withIcon(PhosphorUsersThreeIcon);
+export const SunIcon = withIcon(PhosphorSunIcon);
+export const MoonIcon = withIcon(PhosphorMoonIcon);
+export const SmileyIcon = withIcon(PhosphorSmileyIcon);
+export const SparkleIcon = withIcon(PhosphorSparkleIcon);
+export const LightbulbIcon = withIcon(PhosphorLightbulbIcon);
+export const LightningIcon = withIcon(PhosphorLightningIcon);
+export const HeadCircuitIcon = withIcon(PhosphorHeadCircuitIcon);
+export const HeartIcon = withIcon(PhosphorHeartIcon);
+export const TrashIcon = withIcon(PhosphorTrashIcon);
+export const BellIcon = withIcon(PhosphorBellIcon);
+export const BookOpenIcon = withIcon(PhosphorBookOpenIcon);
+export const BrainIcon = withIcon(PhosphorBrainIcon);
+export const BriefcaseIcon = withIcon(PhosphorBriefcaseIcon);
+export const CalendarBlankIcon = withIcon(PhosphorCalendarBlankIcon);
+export const MenuIcon = withIcon(PhosphorListIcon);
+export const SearchIcon = withIcon(PhosphorMagnifyingGlassIcon);
+export const MicrophoneIcon = withIcon(PhosphorMicrophoneIcon);
+export const PlusIcon = withIcon(PhosphorPlusIcon);
+export const QuotesIcon = withIcon(PhosphorQuotesIcon);
+export const PencilIcon = withIcon(PencilSimpleIcon);
+export const DragHandleIcon = withIcon(DotsSixVerticalIcon);
+export const ChevronLeftIcon = withIcon(CaretLeftIcon);
+export const ChevronRightIcon = withIcon(CaretRightIcon);
+export const GridIcon = withIcon(SquaresFourIcon);
+export const ListBulletsIcon = withIcon(PhosphorListBulletsIcon);
+export const CheckCircleIcon = withIcon(PhosphorCheckCircleIcon);
+export const ChatIcon = withIcon(PhosphorChatIcon);
+export const UploadIcon = withIcon(UploadSimpleIcon);
+export const CameraIcon = withIcon(PhosphorCameraIcon);
+export const SendIcon = withIcon(PaperPlaneTiltIcon);
+export const BarbellIcon = withIcon(PhosphorBarbellIcon);
+export const NoteIcon = withIcon(PhosphorNoteIcon);
+export const TargetIcon = withIcon(PhosphorTargetIcon);
+export const ChartLineIcon = withIcon(PhosphorChartLineUpIcon);
+export const SunHorizonIcon = withIcon(PhosphorSunHorizonIcon);
+export const EnvelopeIcon = withIcon(EnvelopeSimpleIcon);
+export const InfoIcon = withIcon(PhosphorInfoIcon);
+export const LockIcon = withIcon(LockSimpleIcon);
+export const EyeIcon = withIcon(PhosphorEyeIcon);
+export const EyeSlashIcon = withIcon(PhosphorEyeSlashIcon);
